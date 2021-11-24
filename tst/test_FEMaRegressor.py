2f1262a8982cdf3cecace589dc99911d9a6bbbd0
Danillo Roberto Pereira <danillorobertopereirads@gmail.com>
2021-11-24 18:17:12 -0300

    under development

--- tst/test_FEMaRegressor.py
+++ tst/test_FEMaRegressor.py
@@ -12,8 +12,6 @@ from sklearn.metrics import mean_absolute_percentage_error
 import matplotlib.pyplot as plt
 
 
-
-
 df = pd.read_csv('C:\\Users\\coton\\Desktop\\github\\fema\\data\\regressionData.csv')
 
 features = ['MSSubClass', 'LotFrontage', 'LotArea', 'PoolArea', 'MoSold', 'YrSold']	
@@ -21,7 +19,7 @@ target = ['SalePrice']
 
 df = df[features+target].dropna()
 
-train_x, test_x, train_y, test_y = train_test_split(df[features].values, df[target].values, test_size=0.3)
+train_x, test_x, train_y, test_y = train_test_split(df[features].values, df[target].values, test_size=0.1)
 
 scaler = StandardScaler()
 
@@ -29,9 +27,9 @@ train_x = scaler.fit_transform(train_x)
 test_x = scaler.transform(test_x)
 
 
-model = fema_regression.FEMaRegressor(train_x,train_y,1,basis=fema_regression.Basis.radialBasis)
+model = fema_regression.FEMaRegressor(train_x,train_y,3,basis=fema_regression.Basis.radialBasis)
 
-pred = model.predict(test_x,0.01)
+pred = model.predict(test_x,2)
 
 plt.plot(pred,c='r')
 plt.plot(test_y,c='b')
