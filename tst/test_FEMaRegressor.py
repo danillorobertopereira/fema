e2878d4d308048f7fd6e26e8ec16f0170b39bc7b
Danillo Roberto Pereira <danillorobertopereirads@gmail.com>
2021-11-24 18:13:48 -0300

    under development

--- tst/test_FEMaRegressor.py
+++ tst/test_FEMaRegressor.py
@@ -1,14 +1,20 @@
 import sys
+import os
 
-sys.path.insert(1,'../src/fema')
+sys.path.append('C:\\Users\\coton\\Desktop\\github\\fema\\src\\')
 
+
+import fema_regression
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_percentage_error
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('../data/regressionData.csv')
+
+
+
+df = pd.read_csv('C:\\Users\\coton\\Desktop\\github\\fema\\data\\regressionData.csv')
 
 features = ['MSSubClass', 'LotFrontage', 'LotArea', 'PoolArea', 'MoSold', 'YrSold']	
 target = ['SalePrice']
@@ -23,7 +29,7 @@ train_x = scaler.fit_transform(train_x)
 test_x = scaler.transform(test_x)
 
 
-model = FEMaRegressor(train_x,train_y,1,basis=Basis.radialBasis)
+model = fema_regression.FEMaRegressor(train_x,train_y,1,basis=fema_regression.Basis.radialBasis)
 
 pred = model.predict(test_x,0.01)
 
