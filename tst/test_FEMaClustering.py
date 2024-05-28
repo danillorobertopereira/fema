a52ff3f4b1ab995570d3a3bb2d3b3224fff57f92
Danillo Roberto Pereira <danillorobertopereirads@gmail.com>
2024-05-28 16:56:02 -0300

    fema-clustering

--- tst/test_FEMaClustering.py
+++ tst/test_FEMaClustering.py
@@ -16,38 +16,38 @@ import matplotlib.pyplot as plt
 def main():
     clustering = FEMaClustering(z=2)
     
-    N = 30
+    N = 50
     dimensions = 2
-    points = clustering.generate_random_points(bounds=[(0,100),(0,100)],num_points=N)
+    points = clustering.generate_random_points(bounds=[(0,200),(0,200)],num_points=N)
     #clustering.plot_points(points)
 
     clustering.fit(points)
     
     # Plotar pontos
-    plt.figure(figsize=(8, 6))
-    plt.scatter(clustering.samples[:,0], clustering.samples[:,1], color='blue', label='Nuvem de Pontos Inicial (NP)')
-    plt.scatter(clustering.random_samples[:,0], clustering.random_samples[:,1], color='red', label='Pontos Aleatórios')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Nuvem de Pontos Inicial com Pontos Aleatórios')
-    #plt.legend()
-    plt.grid(True)
-    plt.show()
-
-
+    if dimensions == 2:
+        plt.figure(figsize=(8, 6))
+        plt.scatter(clustering.samples[:,0], clustering.samples[:,1], color='blue', label='Nuvem de Pontos Inicial (NP)')
+        plt.scatter(clustering.random_samples[:,0], clustering.random_samples[:,1], color='red', label='Pontos Aleatórios')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Nuvem de Pontos Inicial com Pontos Aleatórios')
+        plt.grid(True)
+        plt.show()
+    
     print(clustering.predict(th_same_cluster=0.85))
 
-    fig, ax = plt.subplots()
+    if dimensions == 2:
+        fig, ax = plt.subplots()
 
-    # Plot the generated points
-    ax.scatter(points[:, 0], points[:, 1], c=clustering.labels, marker='o', edgecolors='black')
-    ax.grid(True)
+        # Plot the generated points
+        ax.scatter(points[:, 0], points[:, 1], c=clustering.labels, marker='o', edgecolors='black')
+        ax.grid(True)
 
-    for i, txt in enumerate(range(N)):
-        ax.annotate(clustering.labels[i], (points[i,0], points[i,1]))
+        for i, txt in enumerate(range(N)):
+            ax.annotate(clustering.labels[i], (points[i,0], points[i,1]))
 
-    plt.show()
-
+        plt.show()
+        
 
 
 if __name__ == "__main__":
